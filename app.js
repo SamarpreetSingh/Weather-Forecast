4dbf270ebba55760470089f29c523478f9dccdc7
SamarpreetSingh <samar.guraya18@gmail.com>
2020-09-04 00:15:53 +0530

    I am stupid

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const fetch = require('node-fetch');
-const port = 3000 | process.env.PORT;
+const port = process.env.PORT | 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
