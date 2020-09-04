e64c8b3a4972f7e12858cbdcd2fe529878d960a2
SamarpreetSingh <samar.guraya18@gmail.com>
2020-09-04 23:38:26 +0530

    kjsdhfk

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ app.use(express.urlencoded({extended: true}));
 
 
 app.get('/', (req, res) => {
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(ip);
    fetch('http://api.ipstack.com/check?access_key='+process.env.location_apikey)
    .then(responce => responce.json())
    .then(data => {
